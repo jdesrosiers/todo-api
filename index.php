7636e9cd90faad0197a57eb2efdddf6fe2d5a199
Jason Desrosiers <jdesrosi@gmail.com>
2018-03-25 20:46:36 -0700

    Update silex-cors-provider and add Allow to index

--- index.php
+++ index.php
@@ -13,9 +13,10 @@ $app["debug"] = true;
 $static = new FileCache(__DIR__ . "/static");
 
 // Register Controllers
-$app->get("/", new GetResourceController($static));
+$app->get("/", new GetResourceController($static))
+    ->after($app["allow"]);
 
 // Initialize CORS support
-$app->after($app["cors"]);
+$app["cors-enabled"]($app);
 
 $app->run();
