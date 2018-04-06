91b35e33deefef1e7f77a3152ccefe6d204f5a5e
Jason Desrosiers <jdesrosi@gmail.com>
2018-04-06 09:55:01 -0700

    Add dummy link to index page

--- index.php
+++ index.php
@@ -4,17 +4,23 @@ use Doctrine\Common\Cache\FilesystemCache;
 use JDesrosiers\Resourceful\Controller\GetResourceController;
 use JDesrosiers\Resourceful\Resourceful;
 use JDesrosiers\Resourceful\FileCache\FileCache;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . "/vendor/autoload.php";
 
 $app = new Resourceful();
 $app["debug"] = true;
+$app["cors.exposeHeaders"] = "if-modified-since location link";
 
 $static = new FileCache(__DIR__ . "/static");
 
 // Register Controllers
 $app->get("/", new GetResourceController($static))
-    ->after($app["allow"]);
+    ->after($app["allow"])
+    ->after(function (Request $request, Response $response) {
+        $response->headers->set("Link", "</foo>; rel=\"foo\"");
+    });
 
 // Initialize CORS support
 $app["cors-enabled"]($app);
