89438198a4ff74a666cb09f3ceac1d29427eda0f
Jason Desrosiers <jdesrosi@gmail.com>
2018-04-11 23:18:09 -0700

    Add a link to a TODO List

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-use Doctrine\Common\Cache\FilesystemCache;
+use JDesrosiers\Resourceful\Controller\DeleteResourceController;
 use JDesrosiers\Resourceful\Controller\GetResourceController;
-use JDesrosiers\Resourceful\Resourceful;
 use JDesrosiers\Resourceful\FileCache\FileCache;
+use JDesrosiers\Resourceful\Resourceful;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -11,17 +11,22 @@ require __DIR__ . "/vendor/autoload.php";
 
 $app = new Resourceful();
 $app["debug"] = true;
-$app["cors.exposeHeaders"] = "if-modified-since, location, link";
+$app["cors.exposeHeaders"] = "allow, if-modified-since, link, location";
 
+$data = new FileCache(__DIR__ . "/data");
 $static = new FileCache(__DIR__ . "/static");
 
 // Register Controllers
 $app->get("/", new GetResourceController($static))
     ->after($app["allow"])
     ->after(function (Request $request, Response $response) {
-        $response->headers->set("Link", "</foo>; rel=\"foo\"; title=\"Foo\"", false);
-        $response->headers->set("Link", "</bar>; rel=\"bar\"; title=\"Bar\"", false);
+        $rel = $request->getSchemeAndHttpHost() . "/rel/todo";
+        $response->headers->set("Link", "</todo/1>; rel=\"$rel\"; title=\"TODO List\"", false);
     });
+
+$app->get("/todo/{id}", new GetResourceController($data))
+    ->after($app["allow"]);
+$app->delete("/todo/{id}", new DeleteResourceController($data));
 
 // Initialize CORS support
 $app["cors-enabled"]($app);
