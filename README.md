82a8493e07cd81de43f1a06c33473dbb51084775
Jason Desrosiers <jdesrosi@gmail.com>
2018-06-19 22:47:28 -0700

    Add listing support

--- README.md
+++ README.md
@@ -3,33 +3,24 @@
 TODO API
 ========
 
-The TODO API was created to showcase a Hypermedia API.
+The TODO API was created to showcase a Hypermedia API. This API is built using
+the Resourceful framework to declaratively define the API with JSON Hyper-Schema
+(draft-04).
 
 Development
 ------------
 
-This API is built using the Resourceful framework.
-
-* `php -S localhost:8000 index.php` - Run the server
+```bash
+docker-compose up
+```
 
 ### Tests
 
-What, no tests?! This is sacrilege!
-
-Resourceful allows us to declaratively define an API with almost no code. Until
-there is need for actual code, we will put off setting up a test framework.
+```bash
+docker-compose run web vendor/bin/phpunit
+```
 
 Deployment
 ----------
 
-Continuous deployment is used for this application. Continuous deployment aims to automate the process of deploying code. Each stage in the process is automated, so that every change that passes all the tests is deployed to production automatically. These are the steps in the process:
-
-* A PR is opened for a branch (feature, bug fix, etc.)
-* Travis CI checks the build
-* The PR is approved
-* The branch is merged into master
-* Travis CI deploys the app to Heroku
-
-This app is deployed to: https://hypermedia-todo.herokuapp.com/
-
-**Note**: Deployment should never be done manually. It should only be done through the automated process.
+The master branch is automatically deployed to: https://hypermedia-todo.herokuapp.com/
--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 
-use Doctrine\Common\Cache\MongoDBCache;
+use JDesrosiers\HypermediaTasks\MongoDbCache;
 use JDesrosiers\Resourceful\CrudControllerProvider\CrudControllerProvider;
 use JDesrosiers\Resourceful\FileCache\FileCache;
 use JDesrosiers\Resourceful\IndexControllerProvider\IndexControllerProvider;
 use JDesrosiers\Resourceful\Resourceful;
 use JDesrosiers\Resourceful\ResourcefulServiceProvider\ResourcefulServiceProvider;
 use JDesrosiers\Resourceful\SchemaControllerProvider\SchemaControllerProvider;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 require __DIR__ . "/vendor/autoload.php";
 
@@ -18,8 +21,6 @@ $app->register(new ResourcefulServiceProvider(), [
     "resourceful.schema-dir" => __DIR__
 ]);
 
-$db = getenv("MONGODB_DBNAME");
-$mongo = (new MongoDB\Client(getenv("MONGODB_URI")))->$db;
 $static = new FileCache(__DIR__ . "/static");
 
 // Register Supporting Controllers
@@ -28,8 +29,24 @@ $app->flush();
 $app->mount("/", new IndexControllerProvider($static));
 
 // Register Controllers
-$data = new MongoDBCache($mongo->task);
+$dbUri = getenv("MONGODB_URI");
+$dbname = getenv("MONGODB_DBNAME");
+$tasks = (new MongoDB\Client($dbUri))->$dbname->{"/schema/task"};
+$data = new MongoDbCache($dbUri, $dbname, "/schema/task");
 $app->mount("/task", new CrudControllerProvider("task", $data));
+$app->get("/task/", function (Resourceful $app, Request $request) use ($tasks) {
+    $app["json-schema.describedBy"] = "/schema/task-list";
+
+    $resource = [
+        "list" => array_map(function ($task) use ($data) {
+            unset($task["_id"]);
+            return $task;
+        }, $tasks->find()->toArray())
+    ];
+
+    $response = JsonResponse::create($resource);
+    return $app["allow"]($request, $response, $app);
+});
 
 // Initialize CORS support
 $app["cors-enabled"]($app);
