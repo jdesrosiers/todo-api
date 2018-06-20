2b3406a20553ff234fff9cd235bacaee6be5eb14
Jason Desrosiers <jdesrosi@gmail.com>
2018-06-19 21:17:25 -0700

    Use a custom MongoDbCache

---
+++ src/MongoDbCache.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace JDesrosiers\HypermediaTasks;
+
+use Doctrine\Common\Cache\Cache;
+use MongoDB\Client;
+
+class MongoDbCache implements Cache
+{
+    private $collection;
+
+    public function __construct($uri, $dbname, $collectionName)
+    {
+        $client = new Client($uri);
+        $this->collection = $client->$dbname->$collectionName;
+    }
+
+    public function fetch($id)
+    {
+        $document = $this->collection->findOne(["_id" => $id]);
+        unset($document["_id"]);
+
+        return $document === null ? false : iterator_to_array($document);
+    }
+
+    public function contains($id)
+    {
+        return $this->fetch($id) !== false;
+    }
+
+    public function save($id, $data, $lifeTime = null)
+    {
+        $this->collection->replaceOne(["_id" => $id], $data, ["upsert" => true]);
+
+        return true;
+    }
+
+    public function delete($id)
+    {
+        $this->collection->deleteOne(["_id" => $id]);
+
+        return true;
+    }
+
+    public function getStats()
+    {
+        return null;
+    }
+}
---
+++ src/MongoDbCacheTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace JDesrosiers\HypermediaTasks;
+
+use MongoDB\Client;
+use PHPUnit\Framework\TestCase;
+
+class MongoDbCacheTest extends TestCase
+{
+    private static $client;
+    private static $service;
+
+    public static function setUpBeforeClass()
+    {
+        $dbUri = getenv("MONGODB_URI");
+        self::$client = new Client($dbUri);
+        self::$service = new MongoDbCache($dbUri, "test", "testCollection");
+        self::resetDb();
+    }
+
+    public function tearDown()
+    {
+        self::resetDb();
+    }
+
+    private static function resetDb()
+    {
+        self::$client->test->testCollection->deleteMany([]);
+    }
+
+    public function testStoreNewObject()
+    {
+        $document = ["foo" => "bar"];
+        $this->assertTrue(self::$service->save("some-id", $document));
+        $this->assertEquals($document, self::$service->fetch("some-id"));
+    }
+
+    public function testReplaceObject()
+    {
+        $original = ["foo" => "bar"];
+        $updated = ["foo" => "abc123"];
+        $this->assertTrue(self::$service->save("some-id", $original));
+        $this->assertTrue(self::$service->save("some-id", $updated));
+        $this->assertEquals($updated, self::$service->fetch("some-id"));
+    }
+
+    public function testRetrieveNonexistentObject()
+    {
+        $this->assertFalse(self::$service->fetch("some-id"));
+    }
+
+    public function testDeleteObject()
+    {
+        $this->assertTrue(self::$service->save("some-id", ["foo" => "bar"]));
+        $this->assertTrue(self::$service->delete("some-id"));
+        $this->assertFalse(self::$service->fetch("some-id"));
+    }
+
+    public function testDeleteNonExistentObject()
+    {
+        $this->assertTrue(self::$service->delete("some-id"));
+    }
+
+    public function testGetStats()
+    {
+        $this->assertEquals(null, self::$service->getStats());
+    }
+}
